2b71010433ebd420adda799631d886b2d167c1f2
egfiuz <123573339+egfiuz@users.noreply.github.com>
2023-04-18 18:12:36 +0000

    Add JobItem

--- main.py
+++ main.py
@@ -2,9 +2,44 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': ' Bengaluru, India',
+    'salary': 'R$ 1.500,00'
+  },
+
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': ' Delhi, India',
+    'salary': 'R$ 1.800,00'
+    
+  },
+
+  {
+    'id': 3,
+    'title': 'Front Enginieer',
+    'location': 'Remote',
+    'salary': 'R$ 2.500,00'
+    
+  }, 
+
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': ' Remote',
+    
+  }
+  
+]
+
 @app.route("/")
 def hello_jovian():
-  return render_template('home.html')
+  return render_template('home.html',
+                        jobs=JOBS,
+                        company_name='EGFiuza')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
